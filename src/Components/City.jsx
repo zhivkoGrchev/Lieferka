f9a6d85f37fc89b2fa1656cc2d8bf2e7e24777a6
zhivkoGrchev <z.grcev@gmail.com>
2024-04-02 00:24:34 +0200

    v1.13

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -2,7 +2,6 @@ import GastroItem from "./GastroItem"
 import PropTypes from 'prop-types'
 import { items } from './BeelitzGastroData.jsx';
 
-
 export default function City ({nameCity, slogan}) {
 
   const ListGastro = items.map(item =>
@@ -17,7 +16,6 @@ export default function City ({nameCity, slogan}) {
     </div>
     )
 
-
   return (
         <>
         <div className="text-center drop-shadow-lg">
@@ -26,7 +24,20 @@ export default function City ({nameCity, slogan}) {
             <h2 className=" text-3xl font-bold pt-6 pb-2 text-slate-700">{nameCity}</h2>
             <p className=" text-slate-700">{slogan}</p>
           </div>
-          <div className="flex flex-wrap px-36 py-4 gap-5 justify-center">{ListGastro}</div>
+
+          <div className=" text-1xl font-bold pt-1 pb-2 text-slate-700">Lieferservice für Speisen und Getränke</div>
+          <div className=" text-1xl font-bold pt-1 pb-2 text-teal-400">Lieferzeiten ab 01.04.24 bis 07.04.24</div>
+          <div>
+            <p><strong>Montag:</strong> Keine Lieferung möglich</p>
+            <p><strong>Dienstag:</strong> ab 10 bis 14 und ab 18 bis 24 Uhr</p>
+            <p><strong>Mittwoch:</strong> ab 10 bis 14 und ab 18 bis 24 Uhr</p>
+            <p><strong>Donnerstag:</strong> ab 18 bis 24 Uhr</p>
+            <p><strong>Freitag:</strong> ab 10 bis 14 Uhr</p>
+            <p><strong>Samstag:</strong> ab 22 bis 01 Uhr</p>
+            <p><strong>Sonntag:</strong> ab 22 bis 01 Uhr</p>
+          </div>
+
+          <div className="flex flex-wrap px-36 py-4 gap-5 justify-center">{/*{ListGastro}*/}</div>
         </div>
         </>
   )
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className="text-center pt-20 font-bold drop-shadow-lg">
       <p className="hover:text-teal-400">Developer: Zhivko Grchev</p>
       <p className="py-5 hover:text-teal-400">e-Mail: z.grcev@gmail.com</p>
-      <p className="hover:text-teal-400">Tel: 0049 15257539557</p>
+      <p className="hover:text-teal-400">Tel: 0049 1525 7539557</p>
     </div>
   )
 }
